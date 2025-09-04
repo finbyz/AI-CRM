d85c584a530e08cf61110b7827dd385a06d63b61
Finbyz <info@finbyz.tech>
2025-09-04 06:36:35 +0000

    fix: Update revise_post method to retrieve revise agent from Content Hub settings

--- ai_crm/social_media/doctype/social_media_post/social_media_post.py
+++ ai_crm/social_media/doctype/social_media_post/social_media_post.py
@@ -89,9 +89,8 @@ class SocialMediaPost(Document):
 
 	@frappe.whitelist()
 	def revise_post(self,instruction:str):
-		content_hub = frappe.get_doc("Content Hub",self.content_hub)
-		linkedin_doc = frappe.get_doc(content_hub.credential_type,content_hub.credential)
-		revise_agent = linkedin_doc.revise_post_agent
+		content_hub_setting = frappe.get_single("Content Hub Setting")
+		revise_agent = content_hub_setting.revise_post_agent
 		ai_input_data = {
 			"title": self.title,
 			"social_media": self.platform,
