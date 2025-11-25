ff68da5c8bac4c7e5f5b8b3fcfd41d25dfd2c4af
Finbyz <info@finbyz.tech>
2025-11-25 05:50:04 +0000

    smart lead followup move to finbyzreach app

--- ai_crm/ai_crm/doctype/lead_followup_setting/test_lead_followup_setting.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, sandeep and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestLeadFollowupSetting(FrappeTestCase):
-	pass
