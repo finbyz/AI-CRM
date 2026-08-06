f0c304f3f0e7ae8196f0d7161cf6b939b2b56a55
Mehul Talpada <mehul.talpada@finbyz.tech>
2026-08-06 12:28:24 +0000

    delete unwanted doctypes

--- ai_crm/social_media/doctype/post_idea_item/post_idea_item.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, sandeep and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class PostIdeaItem(Document):
-	pass
